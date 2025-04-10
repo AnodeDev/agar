deb7ed631b6bcac35e3a1ebdfdd176836d7f01bf
Dexter Hedman <dexterhedman05@proton.me>
2025-04-10 19:00:01 +0200

    Added cursor position update function

--- src/term.zig
+++ src/term.zig
@@ -4,19 +4,33 @@ const builtin = @import("builtin");
 const posix = std.posix;
 const linux = std.os.linux;
 
+const Cursor = struct {
+    row: usize,
+    col: usize,
+
+    fn init() Cursor {
+        return Cursor{
+            .row = 0,
+            .col = 0,
+        };
+    }
+};
+
 // Main struct that holds information about the TTY and its state
-const Term = struct {
+pub const Term = struct {
     default_state: posix.termios, // Original TTY state
     stdin: std.fs.File, // Holds the TTY handle, which is required by Termios
     stdout: std.fs.File,
     size: posix.winsize, // The size of the TTY
+    cursor: Cursor,
 
-    fn init() !Term {
+    pub fn init() !Term {
         const stdin = std.io.getStdIn();
 
         // Checks if the current handle is a TTY, otherwise it throws an error
         if (posix.isatty(stdin.handle)) {
             const stdout = std.io.getStdOut();
+            const cursor = Cursor.init();
             const winsize = posix.winsize{ // Creates an empty winsize to be filled later
                 .row = 0,
                 .col = 0,
@@ -41,9 +55,11 @@ const Term = struct {
                 .stdin = stdin,
                 .stdout = stdout,
                 .size = winsize,
+                .cursor = cursor,
             };
 
             try term.assignSize(); // Fills the winsize
+            try term.resetCursor(); // Makes sure it always start at (0, 0)
 
             return term;
         } else {
@@ -51,17 +67,17 @@ const Term = struct {
         }
     }
 
-    fn deinit(self: *const Term) void {
+    pub fn deinit(self: *const Term) void {
         posix.tcsetattr(self.stdin.handle, .NOW, self.default_state) catch {}; // Applies the original state to the TTY
     }
 
     // Clears the entire screen
-    fn clear(self: *const Term) !void {
+    pub fn clear(self: *const Term) !void {
         try self.stdout.writer().writeAll("\x1b[2J");
     }
 
     // Assigns the TTY size to the struct field 'size'
-    fn assignSize(self: *Term) !void {
+    pub fn assignSize(self: *Term) !void {
         if (builtin.target.os.tag == .linux) {
             // Checks the result of 'ioctl' and returns the appropriate value
             // TODO: Add a proper unexpected return to include the error code
@@ -75,29 +91,68 @@ const Term = struct {
     }
 
     // Brings the cursor back to (0, 0)
-    fn resetCursor(self: *Term) !void {
+    pub fn resetCursor(self: *Term) !void {
         try self.stdout.writer().writeAll("\x1b[H");
+        try self.updateCursorPosition();
     }
 
-    fn moveCursorLeft(self: *Term, distance: usize) !void {
+    // TODO: Add boundary checks to make sure the cursor isn't moved out of bounds
+    pub fn moveCursorLeft(self: *Term, distance: usize) !void {
         try self.stdout.writer().print("\x1b[{d}D", .{ distance });
+        try self.updateCursorPosition();
     }
 
-    fn moveCursorDown(self: *Term, distance: usize) !void {
+    pub fn moveCursorDown(self: *Term, distance: usize) !void {
         try self.stdout.writer().print("\x1b[{d}B", .{ distance });
+        try self.updateCursorPosition();
     }
 
-    fn moveCursorUp(self: *Term, distance: usize) !void {
+    pub fn moveCursorUp(self: *Term, distance: usize) !void {
         try self.stdout.writer().print("\x1b[{d}A", .{ distance });
+        try self.updateCursorPosition();
     }
 
-    fn moveCursorRight(self: *Term, distance: usize) !void {
+    pub fn moveCursorRight(self: *Term, distance: usize) !void {
         try self.stdout.writer().print("\x1b[{d}C", .{ distance });
+        try self.updateCursorPosition();
     }
 
-    fn moveCursorToCoords(self: *Term, x: usize, y: usize) !void {
+    pub fn moveCursorToCoords(self: *Term, x: usize, y: usize) !void {
         if (x > self.size.col or y > self.size.row) return error.OutOfRange;
 
         try self.stdout.writer().print("\x1b[{d};{d}H", .{ x, y });
+        try self.updateCursorPosition();
+    }
+
+    pub fn updateCursorPosition(self: *Term) !void {
+        const stdout = self.stdout.writer();
+        const stdin = self.stdin.reader();
+
+        try stdout.writeAll("\x1b[6n");
+        var buf: [32]u8 = undefined;
+        const bytes_read = try stdin.read(&buf);
+
+        const response = buf[0..bytes_read];
+
+        if (response.len < 5) return error.InvalidResponse;
+
+        if (response[0] != 0x1b or response[1] != '[' or response[bytes_read-1] != 'R') {
+            return error.InvalidResponse;
+        }
+
+        const data = response[2..bytes_read-1];
+        const semicolon = std.mem.indexOfScalar(u8, data, ';') orelse return error.InvalidResponse;
+
+        const row_str = data[0..semicolon];
+        const col_str = data[semicolon+1..];
+
+        const row = try std.fmt.parseInt(usize, row_str, 10);
+        const col = try std.fmt.parseInt(usize, col_str, 10);
+
+        self.cursor.row = row;
+        self.cursor.col = col;
     }
 };
+
+// TODO:
+//  - Add getCursorPosition function
